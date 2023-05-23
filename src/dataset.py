d304bc203d1b7b90ee65e302a3dcece0dc311a3f
Fta98 <fukushimatakuya.contact@gmail.com>
2023-05-23 10:54:46 +0900

    first baseline commit

---
+++ src/dataset.py
@@ -0,0 +1,52 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class SinglelabelDataset(Dataset):
+    def __init__(self, tokenizer, texts, label, max_length=512, label2id=None, train=True):
+        self.texts = texts
+        self.inputs = tokenizer(texts, max_length=max_length, padding='max_length', truncation=True)
+        self.label = label
+        if label2id:
+            self.label = [label2id[v] for v in self.label]
+        self.train = train
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        ids = torch.tensor(self.inputs['input_ids'][idx], dtype=torch.long)
+        mask = torch.tensor(self.inputs['attention_mask'][idx], dtype=torch.long)
+    
+        if self.train:
+            label = torch.tensor(self.label[idx], dtype=torch.int64)
+            return {'ids': ids, 'mask': mask, 'label': label}
+        else:
+            return {'ids': ids, 'mask': mask}
+
+class MultilabelDataset(Dataset):
+    def __init__(self, tokenizer, texts, labelT=None, labelN=None, labelM=None, max_length=512, train=True):
+        self.texts = texts
+        self.inputs = tokenizer(texts, max_length=max_length, padding='max_length', truncation=True)
+        labelT2id = {1: 0, 2: 1, 3: 2, 4: 3}
+        self.labelT = [labelT2id[v] for v in labelT]
+        self.labelN = labelN
+        self.labelM = labelM
+        self.train = train
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        ids = torch.tensor(self.inputs['input_ids'][idx], dtype=torch.long)
+        mask = torch.tensor(self.inputs['attention_mask'][idx], dtype=torch.long)
+    
+        if self.train:
+            labelT = torch.tensor(self.labelT[idx], dtype=torch.int64)
+            labelN = torch.tensor(self.labelN[idx], dtype=torch.int64)
+            labelM = torch.tensor(self.labelM[idx], dtype=torch.int64)
+            return {'ids': ids, 'mask': mask, 'labelT': labelT, 'labelN': labelN, 'labelM': labelM}
+        else:
+            return {'ids': ids, 'mask': mask}
+        
+        
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+import os
+import random
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from sklearn.metrics import f1_score, accuracy_score
+import torch
+
+
+def load_dataset(data_dir, label_file_name='label.csv'):
+    df = pd.read_csv(data_dir.joinpath(label_file_name))
+    df['text'] = None
+    for idx, id in enumerate(df['ID']):
+        text = Path(data_dir).joinpath(f'{id}.txt').read_text()
+        df.loc[idx, 'text'] = text
+    return df
+
+def set_seed(seed=0):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def display_TNM_score(TNMlabels, predsT, predsN, predsM):
+    preds = [str(int(t)) + str(int(n)) + str(int(m)) for t, n, m in zip(predsT, predsN, predsM)]
+    f1 = f1_score(TNMlabels, preds, average='macro')
+    acc = accuracy_score(TNMlabels, preds)
+    print(f'TNM | macro F1: {f1:.4f} Accuracy: {acc:.4f}')
